c0a68e3f3a3a5c88c72c230103ad7eff0ae15a62
nam <namsupermen06@gmail.com>
2023-07-08 16:56:03 +0700

    call api to get all contacts

--- document/library/library.md
+++ document/library/library.md
@@ -1,5 +1,4 @@
-| Stt           |      Name      |           Des |                                       Link |
-| :------------ | :------------: | ------------: | -----------------------------------------: |
-| 1             | **vue-router** | router screen | https://router.vuejs.org/installation.html |
-| col 2 is      |    centered    |           $12 |                                        $12 |
-| zebra stripes |    are neat    |            $1 |                                         $1 |
+| Stt |      Name      |           Des |                                       Link |
+| :-- | :------------: | ------------: | -----------------------------------------: |
+| 1   | **vue-router** | router screen | https://router.vuejs.org/installation.html |
+| 2   |   **axios**    |      call api |          https://axios-http.com/docs/intro |
--- src/components/ContactList.vue
+++ src/components/ContactList.vue
@@ -1,11 +1,55 @@
 <template>
-  <div>
-    <h1>Contact List</h1>
+  <div class="container">
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Name</th>
+          <th scope="col">Email</th>
+          <th scope="col">Designation</th>
+          <th scope="col">Contact No</th>
+        </tr>
+      </thead>
+      <tbody v-for="contact in contacts" :key="contact?.id">
+        <tr class="table-second">
+          <td scope="row">{{ contact?.id }}</td>
+          <td>{{ contact?.name }}</td>
+          <td>{{ contact?.email }}</td>
+          <td>{{ contact?.designation }}</td>
+          <td>{{ contact?.contact_no }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "ContactList",
+  data() {
+    return {
+      contacts: Array,
+    };
+  },
+  created() {
+    this.getContacts();
+  },
+  methods: {
+    async getContacts() {
+      let url = "http://127.0.0.1:8000/api/contacts/";
+      try {
+        let response = await axios.get(url);
+        if (response?.status == 200) {
+          this.contacts = response?.data?.contacts;
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+  mounted() {
+    console.log("Component mounted.");
+  },
 };
 </script>
