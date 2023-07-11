220cd3516b6fe9a1845b22ba3bdc839004f459d9
nam <namsupermen06@gmail.com>
2023-07-11 23:45:38 +0700

    create edit form and handle update and delete contact

--- src/components/ContactList.vue
+++ src/components/ContactList.vue
@@ -8,15 +8,32 @@
           <th scope="col">Email</th>
           <th scope="col">Designation</th>
           <th scope="col">Contact No</th>
+          <th scope="col">Action</th>
         </tr>
       </thead>
       <tbody v-for="contact in contacts" :key="contact?.id">
         <tr class="table-second">
-          <td scope="row">{{ contact?.id }}</td>
+          <td>{{ contact?.id }}</td>
           <td>{{ contact?.name }}</td>
           <td>{{ contact?.email }}</td>
           <td>{{ contact?.designation }}</td>
           <td>{{ contact?.contact_no }}</td>
+          <td>
+            <router-link
+              :to="{ name: 'EditContact', params: { id: contact.id } }"
+              class="btn btn-sm btn-info"
+            >
+              Edit
+            </router-link>
+          </td>
+          <td>
+            <button
+              class="btn btn-sm btn-danger"
+              @click.prevent="deleteContact(contact?.id)"
+            >
+              Delete
+            </button>
+          </td>
         </tr>
       </tbody>
     </table>
@@ -42,6 +59,18 @@ export default {
         let response = await axios.get(url);
         if (response?.status == 200) {
           this.contacts = response?.data?.contacts;
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async deleteContact(id) {
+      let url = `http://127.0.0.1:8000/api/contacts/${id}`;
+      try {
+        let response = await axios.delete(url);
+        if (response?.status == 200) {
+          alert(response?.data?.message);
+          this.getContacts();
         }
       } catch (error) {
         console.log(error);
---
+++ src/components/EditContact.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <div class="alert alert-danger mt-4" v-if="errors.length">
+          <ul class="mb-0">
+            <li v-for="(error, index) in errors" :key="index">
+              {{ index + 1 }}. {{ error }}
+            </li>
+          </ul>
+        </div>
+
+        <form @submit.prevent="updateContact">
+          <fieldset>
+            <div class="form-group">
+              <label class="form-label mt-4">Name</label>
+              <input
+                type="text"
+                v-model="formData.name"
+                class="form-control"
+                placeholder="enter name"
+              />
+            </div>
+            <div class="form-group">
+              <label class="form-label mt-4">Email</label>
+              <input
+                type="email"
+                v-model="formData.email"
+                class="form-control"
+                placeholder="enter email"
+              />
+            </div>
+            <div class="form-group">
+              <label class="form-label mt-4">Designation</label>
+              <input
+                type="text"
+                v-model="formData.designation"
+                class="form-control"
+                placeholder="enter designation"
+              />
+            </div>
+            <div class="form-group">
+              <label class="form-label mt-4">Contact no</label>
+              <input
+                type="text"
+                v-model="formData.contact_no"
+                class="form-control"
+                placeholder="enter contact no"
+              />
+            </div>
+
+            <button class="btn btn-primary mt-4">Update</button>
+          </fieldset>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "EditContact",
+  data() {
+    return {
+      formData: {
+        name: "",
+        email: "",
+        designation: "",
+        contact_no: "",
+      },
+      errors: [],
+    };
+  },
+  created() {
+    this.getContact(this.$route.params?.id);
+  },
+  methods: {
+    async getContact(id) {
+      let url = `http://127.0.0.1:8000/api/contacts/${id}`;
+      try {
+        let response = await axios.get(url);
+        if (response?.status == 200) {
+          console.log("data", response?.data?.contact);
+          this.formData = response?.data?.contact;
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async updateContact() {
+      this.errors = [];
+      if (!this.formData.name) {
+        this.errors.push("Name is required");
+      }
+      if (!this.formData.email) {
+        this.errors.push("Email is required");
+      }
+      if (!this.formData.designation) {
+        this.errors.push("Designation is required");
+      }
+      if (!this.formData.contact_no) {
+        this.errors.push("Contact No is required");
+      }
+
+      if (!this.errors.length) {
+        let url = "http://127.0.0.1:8000/api/contacts/";
+        try {
+          let response = await axios.post(url, this.formData);
+          console.log("data", response?.data?.message);
+          if (response?.status == 200) {
+            alert(response?.data?.message);
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    },
+  },
+  mounted: function () {
+    console.log("edit component mounted");
+  },
+};
+</script>
